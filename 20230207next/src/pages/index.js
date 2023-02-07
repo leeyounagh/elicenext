eb699cd189e917e10616a009755b3173cd14fa2d
leesuyoen <dudgk0216@naver.com>
2023-02-07 11:23:40 +0900

    3번째 커밋

--- 20230207next/src/pages/index.js
+++ 20230207next/src/pages/index.js
@@ -6,5 +6,5 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Hello world2</div>;
+  return <div>Hello world3</div>;
 }
